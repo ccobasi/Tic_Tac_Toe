910be59853bf558ed0b517f02d1858fb4b3a3183
notrexxx <notrexx15@gmail.com>
2021-02-16 15:37:16 -0400

    Add outputs for player

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,10 @@
 #!/usr/bin/env ruby
-puts 'Hello World!'
+puts 'Hello Player!'
+puts 'Instuctions:'
+puts '1) Player 1 and Player 2 need to enter their nicknames'
+puts '2) Select po'
+
+
+
+player_1 = $stdin.read
+puts inp
