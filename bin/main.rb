6bbf97baef21d1f8b86067b493003f6dcf69ecd9
notrexxx <notrexx15@gmail.com>
2021-02-16 19:13:59 -0400

    Fix linter errors

--- bin/main.rb
+++ bin/main.rb
@@ -11,49 +11,48 @@ puts "For example, three different win conditions are:\n      [X] [X] [X]\n     
 puts 'PRESS ENTER TO CONTINUE'
 $stdin.noecho(&:gets).chomp
 
-#Inputs the players names
+# Inputs the players names
 puts 'Player 1, enter your Nickname:'
 nick1 = gets.chomp
-player_one = (nick1)
+player_one = nick1
 puts "Welcome: #{player_one}"
 
 puts 'Player 2, enter your Nickname:'
 nick2 = gets.chomp
-player_two = (nick2)
+player_two = nick2
 puts "Welcome: #{player_two}"
 
-#Inputs the players Marks, selects the mark for player one and gisves player two the one thats left
+# Inputs the players Marks, selects the mark for player one and gisves player two the one thats left
 def player_input
-    marker = []
-    until (marker=='X' || marker == 'O')
-        puts 'Player one, do you want to be X or O?'
-        marker = gets.chomp.upcase
-    if marker == 'X' then
-        player_mark1 = (marker)
-    puts "Player 1 mark is #{marker}"
-    elsif marker == 'O' then
-        player_mark1 = (marker)
-        puts "Player 1 mark is #{marker}"
+  marker = []
+  until %w[X O].include?(marker)
+    puts 'Player one, do you want to be X or O?'
+    marker = gets.chomp.upcase
+    case marker
+    when 'X'
+
+      puts "Player 1 mark is #{marker}"
+    when 'O'
+
+      puts "Player 1 mark is #{marker}"
     else
-        puts 'Player one, do you want to be X or O?'
-        marker = gets.chomp.upcase
+      puts 'Player one, do you want to be X or O?'
+      marker = gets.chomp.upcase
     end
-end
+  end
 
-if marker == 'X' then 
-    player_mark2 = 'O'
-
-    puts "Player 2 mark is #{player_mark2}"
-else
-    player_mark2 = 'X'
-    puts "Player 2 mark is #{player_mark2}"
-end
+  player_mark2 = if marker == 'X'
+                   'O'
+                 else
+                   'X'
+                 end
+  puts "Player 2 mark is #{player_mark2}"
 end
 
 output = player_input
 p output
 
-#Begins the game
+# Begins the game
 
 puts '                  ARE YOU READY?!!!'
 puts '             PRESS ENTER TO BEGIN THE GAME'
@@ -61,24 +60,24 @@ $stdin.noecho(&:gets).chomp
 
 puts 'Player 1: select you position'
 pos1 = gets.chomp
-mark_one = (pos1)
+# mark_one = pos1
 puts "Your position to play is: #{pos1}"
 
 puts 'Player 2: select you position'
 pos2 = gets.chomp
-mark_two = (pos2)
+# mark_two = pos2
 puts "Your position to play is: #{pos2}"
 
-#this will display the board with the selected positions
-#and also make an assestment of the win condition
+# this will display the board with the selected positions
+# and also make an assestment of the win condition
 
 puts "\n      [X] [O] [ ]\n      [ ] [ ] [ ]\n      [ ] [ ] [ ]"
 
-puts  '         READY FOR THE NEXT ROUND?'
-puts '           PRESS ENTER TO CONTINUE'
+puts 'READY FOR THE NEXT ROUND?'
+puts 'PRESS ENTER TO CONTINUE'
 $stdin.noecho(&:gets).chomp
 
-#Repeat the cicle until the win condition is fulfiled 
-#Or the cicle gets to 9 iterations with no win condition, its a TIE
+# Repeat the cicle until the win condition is fulfiled
+# Or the cicle gets to 9 iterations with no win condition, its a TIE
 
-#DISPLAY THE WINNER!!! (or tie)
+# DISPLAY THE WINNER!!! (or tie)
