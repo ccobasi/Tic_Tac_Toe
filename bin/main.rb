91c00585968b30c919bffb936b3e8646ffc4278d
notrexxx <notrexx15@gmail.com>
2021-02-16 15:50:03 -0400

    Add board matrix

--- bin/main.rb
+++ bin/main.rb
@@ -8,3 +8,20 @@ puts '2) Select po'
 
 player_1 = $stdin.read
 puts inp
+
+def fill 
+    @pos =  (1..9).to_a
+end
+
+def matrix 
+    puts <<-fill 
+    #{fill [0]} | #{fill [1]} | #{fill [2]}
+
+    #{fill [3]} | #{fill [4]} | #{fill [5]}
+
+    #{fill [6]} | #{fill [7]} | #{fill [8]}
+    fill
+
+    puts matrix
+end
+
