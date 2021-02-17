6e067de9821da4cae76576ddaea2d874db400534
[Chukwuma Obasi] <ccobasi8@gmail.com>
2021-02-17 22:36:29 +0100

    Add win_con method to board page

--- lib/board.rb
+++ lib/board.rb
@@ -11,6 +11,23 @@ class Board
     [#{@cell['4']}]  [#{@cell['5']}]  [#{@cell['6']}]
     [#{@cell['7']}]  [#{@cell['8']}]  [#{@cell['9']}]"
     end
+
+    def win_con(board)
+        win = false
+        conditions = [
+            [board[0], board[1], board[2]], ||
+            [board[3], board[4], board[5]], ||
+            [board[6], board[7], board[8]], ||
+            [board[0], board[3], board[6]], ||
+            [board[1], board[4], board[7]], ||
+            [board[2], board[5], board[8]], ||
+            [board[0], board[4], board[8]], ||
+            [board[2], board[4], board[6]], 
+        ]
+        condition.any? do |element|
+            element.all? {|i| i == 'X' || 'O' then win = true}
+        end
+    end
    
     
 
