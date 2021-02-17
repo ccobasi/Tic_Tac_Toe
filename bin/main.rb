c694ba6e230707822f9ec7d27d430e30bd9aee68
[Chukwuma Obasi] <59647298+Chubie@users.noreply.github.com>
2021-02-17 12:55:16 +0100

    (Bug) Fix error

--- bin/main.rb
+++ bin/main.rb
@@ -73,9 +73,10 @@ def space_check(board, position)
 end
 
 def full_board_check(board)
-  for i (1...10) && space_check(board, i)
-    return false
-  end
+  for i (1...10)
+    if space_check(board, i)
+      return false
+    end
   true
 end
 
