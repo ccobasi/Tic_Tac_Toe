201d54d11b52db41dfffc8dec534782bdb0a82ef
[Chukwuma Obasi] <59647298+Chubie@users.noreply.github.com>
2021-02-17 13:40:03 +0100

    (Bug) Fix reduntant return

--- bin/main.rb
+++ bin/main.rb
@@ -60,9 +60,11 @@ end
 def valid_position
   loop do
     break if (1..9).include?(position) && !board[position - 1].is_a?(String)
+
     puts 'Please enter a valid number from 1 to 9 to be replaced by your symbol' unless (1..9).include?(position)
     position.gets.chomp.to_i
   end  
+  
   position
 end
 
