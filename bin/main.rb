b8e2f5fdcd41d0b06f344b55a7bbfa16c9ab6c92
notrexxx <notrexx15@gmail.com>
2021-02-18 17:42:01 -0400

    (bug) fix linter errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,20 +1,19 @@
 #!/usr/bin/env ruby
 require 'io/console'
 require 'colorize'
-require '../lib/board.rb'
+require '../lib/board'
 puts 'Hello Player!'
 puts 'Instuctions:'
 puts '1)Player 1 and Player 2 need to enter their nicknames'
 puts '2) Player 1 & 2 need to select their marks (X or O)'
 puts '3) Players will need to select their positions to input their marks'
-puts "For example X on position 1 is:"   
+puts 'For example X on position 1 is:'
 puts "      [X] [ ] [ ]\n      [ ] [ ] [ ]\n      [ ] [ ] [ ]".yellow
 puts '4) In order to win, you need to connect 3 of your marks in a line'
-puts "For example, three different win conditions are:"
+puts 'For example, three different win conditions are:'
 puts "      [X] [X] [X]\n      [X] [X] [ ]\n      [X] [ ] [X]".yellow
 puts 'PRESS ENTER TO CONTINUE'
 $stdin.noecho(&:gets).chomp
-
 
 d_matrix = Board.new
 
@@ -31,36 +30,35 @@ puts "Welcome: #{player_two}"
 
 # Inputs the players Marks, selects the mark for player one and gisves player two the one thats left
 
-  marker = []
-  until %w[X O].include?(marker)
+marker = []
+until %w[X O].include?(marker)
+  puts 'Player 1, do you want to be X or O?'
+  marker = gets.chomp.upcase
+  case marker
+  when 'X'
+
+    puts "Player 1 mark is #{marker}"
+  when 'O'
+
+    puts "Player 1 mark is #{marker}"
+  else
     puts 'Player 1, do you want to be X or O?'
     marker = gets.chomp.upcase
-    case marker
-    when 'X'
-
-      puts "Player 1 mark is #{marker}"
-    when 'O'
-
-      puts "Player 1 mark is #{marker}"
-    else
-      puts 'Player 1, do you want to be X or O?'
-      marker = gets.chomp.upcase
-    end
   end
+end
 
-  player_mark2 = if marker == 'X'
-                   'O'
-                 else
-                   'X'
-                 end
-  puts "Player 2 mark is #{player_mark2}"
+player_mark2 = if marker == 'X'
+                 'O'
+               else
+                 'X'
+               end
+puts "Player 2 mark is #{player_mark2}"
 
 # Begins the game
 
 puts '                  ARE YOU READY?!!!'.green
 puts '             PRESS ENTER TO BEGIN THE GAME'.green
 $stdin.noecho(&:gets).chomp
-
 
 turn = 1
 while turn <= 9
@@ -75,42 +73,35 @@ while turn <= 9
   pos = gets.chomp
   d_matrix.change(pos, mark)
 
-
-
   turn += 1
-  arr = [1,2,3,4,5,6,7,8,9]
+  arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   p arr.include?(pos.to_i)
   if arr.include?(pos.to_i)
     if turn.odd?
       puts "\n  After the player 2 move the board now looks like this:\n"
-      puts ''
-      puts d_matrix.matrix_display.yellow
     else
       puts "\n After the player 1 move the board now looks like this:\n"
-      puts ''
-      puts d_matrix.matrix_display.yellow
     end
-    
   else
-      puts 'Invalid input. Enter number between 1-9'.red
-      turn -= 1
-      puts ''
-      puts d_matrix.matrix_display.yellow
+    puts 'Invalid input. Enter number between 1-9'.red
+    turn -= 1
   end
-  
-  while d_matrix.win_con? == true  
+  puts ''
+  puts d_matrix.matrix_display.yellow
+
+  if d_matrix.win_con? == true
     if turn == 10
-      puts 'This is a draw' 
-      bye = true
+      puts 'This is a draw'
+
     elsif turn.odd?
-     puts "Congratulations player #{player_two.blue} you are the winner!!!" 
-      bye = true
+      puts "Congratulations player #{player_two.blue} you are the winner!!!"
+
     else
       puts "Congratulations player #{player_one.blue} you are the winner!!!"
-     bye = true
+
     end
-    break
+    bye = true
+
   end
- if bye == true then break
- end
+  break if bye == true
 end
--- lib/board.rb
+++ lib/board.rb
@@ -1,47 +1,48 @@
+# rubocop:disable Metrics/CyclomaticComplexity
+# rubocop:disable Metrics/PerceivedComplexity
+# rubocop:disable Metrics/AbcSize
+
 class Board
-    attr_accessor :cell
+  attr_accessor :cell
 
+  def initialize
+    @cell = { '1' => "\s", '2' => "\s", '3' => "\s", '4' => "\s", '5' => "\s", '6' => "\s", '7' => "\s",
+              '8' => "\s", '9' => "\s" }
+  end
 
-    def initialize 
-        @cell = { '1' => "\s", '2' => "\s", '3' => "\s", '4' => "\s", '5' => "\s", '6' => "\s", '7' => "\s", '8' => "\s", '9' => "\s" }
-    
-        
-    end
-
-    def matrix_display 
+  def matrix_display
     "    [#{@cell['1']}]  [#{@cell['2']}]  [#{@cell['3']}]
     [#{@cell['4']}]  [#{@cell['5']}]  [#{@cell['6']}]
     [#{@cell['7']}]  [#{@cell['8']}]  [#{@cell['9']}]"
-    end
+  end
 
-    def change(pos, value)
-        @cell[pos] = value
-      end
+  def change(pos, value)
+    @cell[pos] = value
+  end
 
-    def win_con?
-        @win_con = true if
-            #for X
-            @cell.slice('1', '2', '3').values.all?('X') ||
-            @cell.slice('4', '5', '6').values.all?('X') ||
-            @cell.slice('7', '8', '9').values.all?('X') ||
-            @cell.slice('1', '4', '7').values.all?('X') ||
-            @cell.slice('2', '5', '8').values.all?('X') ||
-            @cell.slice('3', '6', '9').values.all?('X') ||
-            @cell.slice('1', '5', '9').values.all?('X') ||
-            @cell.slice('3', '5', '7').values.all?('X') ||
-            #for O
-            @cell.slice('1', '2', '3').values.all?('O') ||
-            @cell.slice('4', '5', '6').values.all?('O') ||
-            @cell.slice('7', '8', '9').values.all?('O') ||
-            @cell.slice('1', '4', '7').values.all?('O') ||
-            @cell.slice('2', '5', '8').values.all?('O') ||
-            @cell.slice('3', '6', '9').values.all?('O') ||
-            @cell.slice('1', '5', '9').values.all?('O') ||
-            @cell.slice('3', '5', '7').values.all?('O')
-       
-    
-    end
-   
-    
-
+  def win_con?
+    @win_con = true if
+        # for X
+        @cell.slice('1', '2', '3').values.all?('X') ||
+        @cell.slice('4', '5', '6').values.all?('X') ||
+        @cell.slice('7', '8', '9').values.all?('X') ||
+        @cell.slice('1', '4', '7').values.all?('X') ||
+        @cell.slice('2', '5', '8').values.all?('X') ||
+        @cell.slice('3', '6', '9').values.all?('X') ||
+        @cell.slice('1', '5', '9').values.all?('X') ||
+        @cell.slice('3', '5', '7').values.all?('X') ||
+        # for O
+        @cell.slice('1', '2', '3').values.all?('O') ||
+        @cell.slice('4', '5', '6').values.all?('O') ||
+        @cell.slice('7', '8', '9').values.all?('O') ||
+        @cell.slice('1', '4', '7').values.all?('O') ||
+        @cell.slice('2', '5', '8').values.all?('O') ||
+        @cell.slice('3', '6', '9').values.all?('O') ||
+        @cell.slice('1', '5', '9').values.all?('O') ||
+        @cell.slice('3', '5', '7').values.all?('O')
+  end
 end
+
+# rubocop:enable Metrics/CyclomaticComplexity
+# rubocop:enable Metrics/PerceivedComplexity
+# rubocop:enable Metrics/AbcSize
