06f0760e2b0a00cbe87a508044f888c41230e4a7
[Chukwuma Obasi] <ccobasi8@gmail.com>
2021-02-25 09:11:05 +0100

    (Add) update to tic_tac_toe_spec Board

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,27 +1,6 @@
 require_relative '../lib/board'
-require_relative '../bin/main'
 
 d_matrix = Board.new
-
-empty_tic_tac_toe = Board.new
-full_tie_tic_tac_toe = Board.new(p1, p2)
-full_tie_tic_tac_toe.d_matrix.matrix_display = %w[O X O X O X X O X]
-win_con1 = Board.new(p1, p2)
-win_con1.d_matrix.matrix_display = ['O', 'O', 'O', 4, 5, 6, 7, 8, 9]
-win_con2 = Board.new(p1, p2)
-win_con2.d_matrix.matrix_display = [1, 2, 3, 'O', 'O', 'O', 7, 8, 9]
-win_con3 = Board.new(p1, p2)
-win_con3.d_matrix.matrix_display = [1, 2, 3, 4, 5, 6, 'O', 'O', 'O']
-win_con4 = Board.new(p1, p2)
-win_con4.d_matrix.matrix_display = ['O', 2, 3, 'O', 5, 6, 'O', 8, 9]
-win_con5 = Board.new(p1, p2)
-win_con5.d_matrix.matrix_display = [1, 'O', 3, 4, 'O', 6, 7, 'O', 9]
-win_con6 = Board.new(p1, p2)
-win_con6.d_matrix.matrix_display = [1, 2, 'O', 4, 5, 'O', 7, 8, 'O']
-win_con7 = Board.new(p1, p2)
-win_con7.d_matrix.matrix_display = ['O', 2, 3, 4, 'O', 6, 7, 8, 'O']
-win_con8 = Board.new(p1, p2)
-win_con8.d_matrix.matrix_display = [1, 2, 'O', 4, 'O', 6, 'O', 8, 9]
 
 describe Board do
   describe '#matrix_display' do
@@ -37,7 +16,7 @@ describe Board do
 
   describe '#change' do
     it 'changes the value of the choosen cell in the array' do
-        expect(d_matrix.change('1', 'X')).to eq('X')
+      expect(d_matrix.change('1', 'X')).to eq('X')
     end
     it 'changes the value of the choosen cell in the array' do
       expect(d_matrix.change('2', 'O')).to eq('O')
@@ -63,37 +42,37 @@ describe Board do
 
   describe '#for_x' do
     it 'return true if 3 marks are X in any row, column or diagonal' do
-      board.change('3', 'X')
-      board.change('6', 'X')
-      board.change('9', 'X')
+      d_matrix.change('3', 'X')
+      d_matrix.change('6', 'X')
+      d_matrix.change('9', 'X')
 
-      expect(board.for_x).to be true
+      expect(d_matrix.for_x).to be true
     end
 
     it 'return false if 3 marks are not X in any row, column or diagonal' do
-      board.change('1', 'X')
-      board.change('2', 'O')
-      board.change('3', 'X')
+      d_matrix.change('1', 'X')
+      d_matrix.change('2', 'O')
+      d_matrix.change('3', 'X')
 
-      expect(board.for_x).to be false
+      expect(d_matrix.for_x).to be false
     end
   end
 
   describe '#for_o' do
     it 'return true if 3 marks are O in any row, column or diagonal' do
-      board.change('1', 'O')
-      board.change('4', 'O')
-      board.change('7', 'O')
+      d_matrix.change('1', 'O')
+      d_matrix.change('4', 'O')
+      d_matrix.change('7', 'O')
 
-      expect(board.for_o).to be true
+      expect(d_matrix.for_o).to be true
     end
 
     it 'return false if 3 marks are not O in any row, column or diagonal' do
-      board.change('1', 'O')
-      board.change('2', 'X')
-      board.change('3', 'O')
+      d_matrix.change('1', 'O')
+      d_matrix.change('2', 'X')
+      d_matrix.change('3', 'O')
 
-      expect(board.for_o).to be false
+      expect(d_matrix.for_o).to be false
     end
   end
 
@@ -105,13 +84,12 @@ describe Board do
   end
 
   describe '#empty' do
-    it 'checks if the cell is empty and return true if cell is not empty' do
-      outcome = @cell[input] == [5]
-      expect(outcome).to be true
+    it 'checks if the cell is empty' do
+      expect(d_matrix.empty('1')).to be nil
     end
-    it 'checks if the cell is empty and return false if cell is empty' do
-      outcome = @cell[input] == "\s"
-      expect(outcome).to be false
+    it 'checks if the cell is empty if not' do
+      d_matrix.change('1', 'X')
+      expect(d_matrix.empty('1')).to be true
     end
   end
 end
